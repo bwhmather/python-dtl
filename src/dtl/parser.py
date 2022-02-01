c91ffc2b718da1006b2fb8a060fc41624a988b77
Ben Mather <bwhmather@bwhmather.com>
2022-02-01 22:03:28 +0000

    Reformat using black

--- src/dtl/parser.py
+++ src/dtl/parser.py
@@ -196,7 +196,15 @@ _generator.register(n.JoinClause, [t.Join, n.TableBinding, n.JoinConstraint])
 
 _generator.register(n.JoinOnConstraint, [t.On, n.Expression])
 
-_generator.register(n.JoinUsingConstraint, [t.Using, t.OpenParen, Annotated[List[n.UnqualifiedColumnName], Delimiter(t.Comma)], t.CloseParen])
+_generator.register(
+    n.JoinUsingConstraint,
+    [
+        t.Using,
+        t.OpenParen,
+        Annotated[List[n.UnqualifiedColumnName], Delimiter(t.Comma)],
+        t.CloseParen,
+    ],
+)
 
 
 # === Filtering ================================================================
